56e290399ddfdb721f739158538d4364d9a6abd2
estebanSulcaInfante <esteban.sulca@utec.edu.pe>
2025-08-18 03:22:25 -0500

    fix: cors allow *

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,18 @@
 # app/extensions.py
 from flask_migrate import Migrate
 from .models import db
+from flask_cors import CORS
 
 migrate = Migrate()
 
 def register_extensions(app):
     db.init_app(app)
     migrate.init_app(app, db)
+    CORS(
+        app,
+        resources={r"/api/*": {"origins": "*"}},
+        supports_credentials=False,
+        allow_headers=["*"],
+        expose_headers=["*"],
+        methods=["GET","POST","PUT","PATCH","DELETE","OPTIONS"]
+    )
